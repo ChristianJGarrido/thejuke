86c0778e8e63082caf2273be9d866cb3b59a600e
christianjgarrido <cgarrid1@binghamton.edu>
2014-08-14 12:17:54 +0100

    CRUD operations for Song (Song.java, SongService.java)

--- src/main/java/com/netbuilder/thejuke/SongService.java
+++ src/main/java/com/netbuilder/thejuke/SongService.java
@@ -3,6 +3,9 @@ package com.netbuilder.thejuke;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.validation.ValidationException;
 
 public class SongService {
 	private EntityManager entityManager;
@@ -35,25 +38,91 @@ public class SongService {
 		}
 	}
 	
-	public List<Song> readAll() {
+	public List<Song> findAllSongs() {
 		
-		List<Song> list = entityManager.createQuery("Select a Song a", Song.class).getResultList();
-		return list;
+        TypedQuery<Song> typedQuery = entityManager.createNamedQuery(Song.FIND_ALL, Song.class);
+        return typedQuery.getResultList();
 		
 	}
 	
-	public Song read(long key) {
+	public Song findSong(final Long key) {
 		
-		return entityManager.find(Song.class, key);
+		if (key == null)
+            throw new ValidationException("Invalid Song id");
+
+        return entityManager.find(Song.class, key);
 	}
 	
-	public void update(long key, Song song) {
+	public Song updateSong(Song song) {
+		if(song == null) {
+			throw new ValidationException("Song object is null");
+		}
 		
-		Song sg = entityManager.find(Song.class, key);
-		
-		entityManager.getTransaction().begin();
-		sg.update(song);
-		entityManager.getTransaction().commit();
-		
+		return entityManager.merge(song);
 	}
+	
+	 public Song createItem(Song song) {
+	        if (song == null)
+	            throw new ValidationException("Song object is null");
+
+	        if (song.getGenre() != null && (Integer)(song.getGenre().getId()) == null) {
+	            entityManager.persist(song.getGenre());
+	        }
+
+	        entityManager.persist(song);
+	        return song;
+	    }
+	
+	public Song read(String name) {
+		
+		Query query = entityManager.createQuery("Select a from Song a where a.name = :sname");
+		query.setParameter("sname", name);
+		return (Song)query.getSingleResult();
+	}
+	
+	public void removeSong(Song song) {
+	        if (song == null)
+	            throw new ValidationException("Song object is null");
+
+	        entityManager.remove(entityManager.merge(song));
+	}
+
+	public void removeItem(Long songId) {
+		if (songId == null)
+	            throw new ValidationException("itemId is null");
+
+	        removeSong(findSong(songId));
+	}
+	
+	 public List<Song> findByGenre(Genre g) {
+		 if (g == null)
+			 throw new ValidationException("Invalid genre");
+
+	     TypedQuery<Song> typedQuery = entityManager.createNamedQuery(Song.FIND_BY_GENRE_ID, Song.class);
+	     	typedQuery.setParameter("genreId", g.getId());
+	     	
+	     return typedQuery.getResultList();
+	 }
+	 
+	 public List<Song> findBySongName(String name) {
+		 if (name == null)
+			 throw new ValidationException("Invalid name");
+
+	     TypedQuery<Song> typedQuery = entityManager.createNamedQuery(Song.FIND_BY_SONG_NAME, Song.class);
+	     	typedQuery.setParameter("songName", name);
+	     	
+	     return typedQuery.getResultList();
+	 }
+	 
+	 public List<Song> searchItems(String keyword) {
+		 if (keyword == null)
+	            keyword = "";
+		 
+		 TypedQuery<Song> typedQuery = entityManager.createNamedQuery(Song.SEARCH, Song.class);
+		 typedQuery.setParameter("keyword", "%" + keyword.toUpperCase() + "%");
+	        
+		 return typedQuery.getResultList();
+	 }
+	 
+	
 }
