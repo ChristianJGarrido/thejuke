bc4268a26cee4887e004de141a77147a137da41b
Alorty <alorty1@gmail.com>
2014-08-14 12:30:19 +0100

    Artist Service with mostly implemented CRUD

--- src/main/java/com/netbuilder/thejuke/Artist.java
+++ src/main/java/com/netbuilder/thejuke/Artist.java
@@ -11,12 +11,19 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="Artist")
+@NamedQueries({
+	@NamedQuery(name=Artist.FIND_ALL, query="SELECT a FROM Artist a"),
+	@NamedQuery(name=Artist.FIND_BY_NAME, query="SELECT a FROM Artist a WHERE a.name=:name"),
+	@NamedQuery(name=Artist.FIND_BY_ALBUM, query="SELECT a FROM Artist a")// TODO: Finish this complex query
+})
 public class Artist {
 
 	@Id
@@ -41,6 +48,10 @@ public class Artist {
 	@Column(name = "bio")
 	private String bio;
 	
+	public static final String FIND_ALL = "Artist.findAll";
+	public static final String FIND_BY_NAME = "Artist.findByName";
+	public static final String FIND_BY_ALBUM = "Artist.findByAlbum";
+	
 	public Artist(String name, String bio){
 		this.name = name;
 		this.bio = bio;
@@ -58,7 +69,6 @@ public class Artist {
 		sb.append("name='").append(name).append("', ");
 		sb.append("bio='").append(bio).append("'}");
 		return sb.toString();
-		
 	}
 	
 	public long getId() {
--- src/main/java/com/netbuilder/thejuke/ArtistService.java
+++ src/main/java/com/netbuilder/thejuke/ArtistService.java
@@ -3,26 +3,92 @@ package com.netbuilder.thejuke;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
+/**
+ * @author Taylor Hunter
+ * Service for persisting and retrieving Artists
+ */
 public class ArtistService {
-	private EntityManager entityManager;
+	private EntityManager em;
 
-	public ArtistService(EntityManager entity) {
-		this.entityManager = entity;
+	/**
+	 * Constructor that supplies a reference to the Entity Manager
+	 * @param entity Entity Manager being used
+	 */
+	public ArtistService(EntityManager em) {
+		this.em = em;
 	}
 	
+	/**
+	 * 
+	 * @param id Desired Artist.id to find
+	 * @return Returns the artist with the matching ID. May be null
+	 */
+	public Artist findArtist(Long id){
+		return em.find(Artist.class, id);
+	}
+
+	/**
+	 * 
+	 * @param name Desired Artist.name to find 
+	 * @return List of all artists with matching name
+	 */
+	public List<Artist> findArtist(final String name){
+		TypedQuery<Artist> tq = em.createNamedQuery(Artist.FIND_BY_NAME, Artist.class);
+		tq.setParameter("name", name);
+		return tq.getResultList();
+	}
+	
+	/**
+	 * 
+	 * @return List of all artists
+	 */
+	public List<Artist> findAllArtists(){
+		TypedQuery<Artist> typedQuery = em.createNamedQuery(Artist.FIND_ALL, Artist.class);
+		return typedQuery.getResultList();
+	}
+	
+	// TODO: public List<Artist> findArtists(Album album){}
+	
+	/**
+	 * Updates the supplied artist within the database if it exists, otherwise creates
+	 * a new record.
+	 * @param artist The modified artist to be updated within the database
+	 * @return The updated artist
+	 */
+	public Artist updateArtist(final Artist artist){
+		em.merge(artist);
+		
+		return artist;
+	}
+	
+	/**
+	 * 
+	 * @param artist Removes an artist by object if it exists, otherwise does nothing.
+	 */
+	public void removeArtist(final Artist artist){
+		em.remove(em.merge(artist));
+	}
+	
+	/**
+	 * Adds the supplied artists to the database
+	 * @param list List of artists to be added
+	 */
 	public void persistArtist(List<Artist> list){
-		entityManager.getTransaction().begin();
+		em.getTransaction().begin();
 		for(Artist artist : list){
-			entityManager.persist(artist);
+			em.persist(artist);
 		}
 		
-		entityManager.getTransaction().commit();
+		em.getTransaction().commit();
 	}
 	
-	
+	/**
+	 * Prints all existing entities within the database
+	 */
 	public void listArtists(){
-		List<Artist> list = entityManager.createQuery("Select a from Artist a", Artist.class).getResultList();
+		List<Artist> list = em.createQuery("Select a from Artist a", Artist.class).getResultList();
 		for(Artist artist : list){
 			System.out.println(artist.toString());
 		}
