0a47a07ff55fea6a961ad77d04d020fa37f29645
BrassRobo <michael.m.basov@gmail.com>
2014-08-27 14:12:09 +0100

    Remade Song Queue after reversion problem. Queue can now have songs added to it. They can be removed by an admin

--- src/main/java/com/netbuilder/thejuke/web/QueueController.java
+++ src/main/java/com/netbuilder/thejuke/web/QueueController.java
@@ -5,8 +5,10 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 
 import com.netbuilder.thejuke.entities.Song;
+@Named
 @SessionScoped
 public class QueueController implements Serializable
 {
@@ -24,13 +26,36 @@ public class QueueController implements Serializable
 	public void setSongQueue(Queue<Song> songQueue) {
 		this.songQueue = songQueue;
 	}
-	public void addSongToQueue(Song song)
+	public String doAddSongToQueue(Song song)
 	{
 		songQueue.add(song);
+		return "#";
 	}
-	public void removeSongFromQueue()
+	public String doRemoveSongFromQueue(Song song)
 	{
+		if(song==null)
+		{
+			System.out.println("Song is null");
+			return "#";
+		}
+		songQueue.remove(song);
+		return "#";
+	}
+	public String doRemoveSongFromQueue()
+	{
+		if(songQueue==null)
+		{
+			System.out.println("Song Queue is null");
+			return "#";
+		}
+		if(songQueue.size()<1)
+		{
+			System.out.println("Song Queue is empty");
+			return "#";
+		}
 		songQueue.remove();
+		System.out.println(songQueue);
+		return "#";
 	}
 
 }
